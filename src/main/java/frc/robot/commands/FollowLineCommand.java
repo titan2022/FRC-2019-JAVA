551756c6613e34c09ebdfc24b79a1d23e613dc4c
SutterButter4 <jsutter909@gmail.com>
2019-01-07 14:03:42 -0600

    Added helper funtions to followLineCommand

--- src/main/java/frc/robot/commands/FollowLineCommand.java
+++ src/main/java/frc/robot/commands/FollowLineCommand.java
@@ -33,6 +33,21 @@ public class FollowLineCommand extends Command {
     protected void execute() {
         System.out.println("FollowLineCommand execute");
     }
+
+    // Called by execute to approach the tape using vision
+    protected void visionStage () {
+
+    }
+    
+    // Called by execute to line up when only 1 sensor has seen tape
+    protected void oneSensorStage () {
+
+    }
+
+    // Called by execute to line up when both sensors see tape
+    protected void twoSensorStage () {
+
+    } 
   
     // Make this return true when this Command no longer needs to run execute()
     @Override
@@ -44,6 +59,11 @@ public class FollowLineCommand extends Command {
     @Override
     protected void end() {
         System.out.println("FollowLineCommand end");
+    }
+
+    // Called for manual interruption of command
+    protected void kill(){
+
     }
   
     // Called when another command which requires one or more of the same
