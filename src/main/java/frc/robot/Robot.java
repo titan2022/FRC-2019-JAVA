187cb3a8bc96b124e30365ade1345eccf714d1de
trajing <trajingplaysgames@gmail.com>
2019-01-09 13:58:38 -0600

    Fix Robot class indentation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,26 +25,26 @@ import frc.robot.subsystems.FollowLineSubsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static OI oi;
+    public static OI oi;
 
-  public static ExampleSubsystem subsystem = new ExampleSubsystem();
-  public static DriveSubsystem driveSubsystem = new DriveSubsystem();
-  public static FollowLineSubsystem followLineSubsystem = new FollowLineSubsystem();
-
-  Command autonomousCommand;
-  SendableChooser<Command> chooser = new SendableChooser<>();
+    public static ExampleSubsystem subsystem = new ExampleSubsystem();
+    public static DriveSubsystem driveSubsystem = new DriveSubsystem();
+    public static FollowLineSubsystem followLineSubsystem = new FollowLineSubsystem();
+  
+    Command autonomousCommand;
+    SendableChooser<Command> chooser = new SendableChooser<>();
 
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
    */
-  @Override
-  public void robotInit() {
-    oi = new OI();
-    chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    SmartDashboard.putData("Auto mode", chooser);
-  }
+    @Override
+    public void robotInit() {
+        oi = new OI();
+        chooser.setDefaultOption("Default Auto", new ExampleCommand());
+        // chooser.addOption("My Auto", new MyAutoCommand());
+        SmartDashboard.putData("Auto mode", chooser);
+    }
 
   /**
    * This function is called every robot packet, no matter the mode. Use
@@ -54,23 +54,23 @@ public class Robot extends TimedRobot {
    * <p>This runs after the mode specific periodic functions, but before
    * LiveWindow and SmartDashboard integrated updating.
    */
-  @Override
-  public void robotPeriodic() {
-  }
+    @Override
+    public void robotPeriodic() {
+    }
 
   /**
    * This function is called once each time the robot enters Disabled mode.
    * You can use it to reset any subsystem information you want to clear when
    * the robot is disabled.
    */
-  @Override
-  public void disabledInit() {
-  }
+    @Override
+    public void disabledInit() {
+    }
 
-  @Override
-  public void disabledPeriodic() {
-    Scheduler.getInstance().run();
-  }
+    @Override
+    public void disabledPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
@@ -83,9 +83,9 @@ public class Robot extends TimedRobot {
    * chooser code above (like the commented example) or additional comparisons
    * to the switch structure below with additional strings & commands.
    */
-  @Override
-  public void autonomousInit() {
-    autonomousCommand = chooser.getSelected();
+    @Override
+    public void autonomousInit() {
+        autonomousCommand = chooser.getSelected();
 
     /*
      * String autoSelected = SmartDashboard.getString("Auto Selector",
@@ -95,42 +95,42 @@ public class Robot extends TimedRobot {
      */
 
     // schedule the autonomous command (example)
-    if (autonomousCommand != null) {
-      autonomousCommand.start();
+        if (autonomousCommand != null) {
+            autonomousCommand.start();
+        }
     }
-  }
 
   /**
    * This function is called periodically during autonomous.
    */
-  @Override
-  public void autonomousPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
+    @Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
     }
-  }
+
+    @Override
+    public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) {
+            autonomousCommand.cancel();
+        }
+    }
 
   /**
    * This function is called periodically during operator control.
    */
-  @Override
-  public void teleopPeriodic() {
-    Scheduler.getInstance().run();
-  }
+    @Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
   /**
    * This function is called periodically during test mode.
    */
-  @Override
-  public void testPeriodic() {
-  }
+    @Override
+    public void testPeriodic() {
+    }
 }
