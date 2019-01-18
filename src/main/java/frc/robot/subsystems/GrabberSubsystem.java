5805389c09b2ff7f7be66c2d347abc2a38b050bf
Brett Looby <bmlooby@gmail.com>
2019-01-17 19:11:28 -0600

    fixed grabber subsystem it was broken

--- src/main/java/frc/robot/subsystems/GrabberSubsystem.java
+++ src/main/java/frc/robot/subsystems/GrabberSubsystem.java
@@ -8,13 +8,42 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 /**
  * Add your docs here.
  */
 public class GrabberSubsystem extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+  private DoubleSolenoid topGrabPiston, hatchReleasePiston;
+  public GrabberSubsystem() {
+    topGrabPiston = new DoubleSolenoid(4, 5);
+    hatchReleasePiston = new DoubleSolenoid(6, 7);
+  }
+
+  public void activateTopGrabPiston() {
+    topGrabPiston.set(Value.kForward);
+  }
+
+  public void reverseTopGrabPiston() {
+    topGrabPiston.set(Value.kReverse);
+  }
+
+  public void deactivateTopGrabPiston() {
+    topGrabPiston.set(Value.kOff);
+  }
+
+  public void activateHatchReleasePiston() {
+    hatchReleasePiston.set(Value.kForward);
+  }
+
+  public void reverseHatchReleasePiston() {
+    hatchReleasePiston.set(Value.kReverse);
+  }
+
+  public void deactivateHatchReleasePiston() {
+    hatchReleasePiston.set(Value.kOff);
+  }
 
   @Override
   public void initDefaultCommand() {
